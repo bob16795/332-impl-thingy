6742c06829cfe2939d4b6370e3ef03bc6ff5acf6
TheMostOGName <themostorigninalname@gmail.com>
2025-04-22 13:00:22 -0400

    Implemented data structure mapping

--- src/structure.c
+++ src/structure.c
@@ -1,1 +1,112 @@
 #include "structure.h"
+
+
+DataMap data_init(char **words, int *shuffle) {
+    DataMap map = {0};
+    int* value = shuffle;
+    Word* head = NULL;
+
+    //Construct linked list
+    for (char** word = words; word; word = word + 1, value = value + 1) {
+        Word* current = malloc(sizeof(Word));
+        current->word = word; 
+        current->value = *value;
+        current->nextWord = head; 
+
+        head = current;
+
+        map.wordCount++;
+    }
+    
+    map.words = head;
+
+    return map;
+}
+
+DataError data_opt(DataMap *map) {
+    
+
+
+    for (Word* word = map->words; word; word = word->nextWord) {
+        for (char* ch = word->word; *ch; ch = ch + 1) {
+            map->ranges[(int) (*ch)].prob++;
+            map->charCount++;
+        }
+    }
+
+    for (int i = 0; i < 256; i++) {
+        if (map->ranges[i].prob != 0) map->uniqueChars++;
+    }
+
+    if (map->uniqueChars == 0) {
+        return DATA_ERR_ZEROCHARS;
+    }
+
+    for (int i = 0; i < 256; i++) {
+        map->ranges[i].prob /= map->charCount;
+    } 
+
+    //Begin character assignment to interval of [0, wordCount)
+    int lowInterval = 0;
+
+    //Many will be 0
+    for (int i = 0; i < 256; i++) {
+        //ignore empty values they don't have a character
+        if (map->ranges[i].prob == 0) continue;
+        
+        //set the ranges
+        map->ranges[i].low = lowInterval;
+        map->ranges[i].high = lowInterval + map->ranges[i].prob;
+
+        //reset the lowest available interval value
+        lowInterval = lowInterval + map->ranges[i].prob;
+    }
+
+
+    //Encode each word to a unique value for sub cipher
+    for (Word* word = map->words; word; word = word->nextWord) {
+        //Set the max range of the word
+        Range wordRange = {
+            .low = 0, 
+            .high = 1
+        }; 
+    
+        for (char* ch = *word->word; *ch; ch = ch + 1) {
+            int currentRange = wordRange.high - wordRange.low;
+            Range chRange = map->ranges[(int) (*ch)];
+    
+            //Shrink the range
+            wordRange.high = wordRange.low + (currentRange * chRange.high);
+            wordRange.low = wordRange.low + (currentRange * chRange.low); 
+        }
+
+        word->low = wordRange.low;
+        word->high = wordRange.high;
+
+        //Dummy printout for testing
+        printf("%s || [%.5f, %.5f)\n", *word->word, word->low, word->high);
+        
+    }
+    return DATA_OK;
+}
+
+DataError data_store(DataMap *const map, FILE *stream) {
+
+}
+
+// runtime fns
+DataMap data_load(FILE *stream) {
+
+}
+
+char const *data_access(DataMap *const map, char *const input) {
+
+
+}
+
+// shared
+void data_deinit(DataMap *const map) {
+    
+}
+
+
--- src/structure.h
+++ src/structure.h
@@ -1,4 +1,53 @@
-#ifndef STRUCTURE_H
-#define STRUCTURE_H
+#ifndef STRUCT_H
+#define STRUCT_H
+
+#include <stdio.h>
+
+#define MAP_SIZE 2048
+
+typedef struct DataMap DataMap;
+typedef struct Word Word;
+typedef struct Range Range;
+
+struct DataMap {
+   Word* words;
+   Range ranges[256]; 
+
+   int uniqueChars;
+   int wordCount;
+   int charCount;
+};
+
+struct Word {
+    char** word;
+    int value;
+    Word* nextWord;
+    //Store the range that can encode the word
+    float low;
+    float high;
+};
+
+struct Range {
+    float prob;
+    float low;
+    float high;
+};
+
+typedef enum {
+  DATA_OK = 0,
+  DATA_ERR_ZEROCHARS,
+} DataError;
+
+// preparation fns
+DataMap data_init(char **words, int *shuffle);
+DataError data_opt(DataMap *map);
+DataError data_store(DataMap *const map, FILE *stream);
+
+// runtime fns
+DataMap data_load(FILE *stream);
+char const *data_access(DataMap *const map, char *const input);
+
+// shared
+void data_deinit(DataMap *const map);
 
 #endif
